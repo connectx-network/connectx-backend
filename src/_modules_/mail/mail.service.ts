e76e3e98d912d2bdb1a36565a7357301c80b59b5
andrew <anntp@twendeesoft.com>
2024-02-26 09:50:06 +0700

    Fix email logo

--- src/_modules_/mail/mail.service.ts
+++ src/_modules_/mail/mail.service.ts
@@ -7,12 +7,18 @@ import { SendMailOptions } from 'nodemailer';
 
 @Injectable()
 export class MailService {
+  private logoUrl = process.env.LOGO_URL;
+
   constructor(private readonly mailerService: MailerService) {}
 
   async sendCreateAccountOtpEmail(data: OtpEmailDto) {
     const { to, subject, otp, fullName } = data;
     const templatePath = resolve(__dirname, 'templates', 'mail.verify.ejs');
-    const renderedHTML = await renderFile(templatePath, { fullName, otp });
+    const renderedHTML = await renderFile(templatePath, {
+      fullName,
+      otp,
+      logoUrl: this.logoUrl,
+    });
     const mailOptions: SendMailOptions = {
       from: process.env.MAIL_ADDRESS,
       to,
